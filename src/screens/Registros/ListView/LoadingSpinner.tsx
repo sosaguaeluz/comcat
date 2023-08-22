69980e3a1a9484907e07069c3fa095e51f070629
Rafael Calpena Rodrigues <rafael.calpena@gmail.com>
2023-08-22 11:12:01 -0300

    feat: fix answer button, add spinner

--- src/screens/Registros/ListView/LoadingSpinner.tsx
+++ src/screens/Registros/ListView/LoadingSpinner.tsx
@@ -1,15 +1,15 @@
 import styled from "styled-components";
 import loadingSpinner from '../../../assets/loading-spinner-3-dots-fade.svg';
 
+const WrapperDiv = styled.div`
+text-align: center;
+padding: 20px;
+width: 100%;
+font-size: 2em;
+background: #fff;
+`
+
 export function LoadingSpinner() {
-
-    const WrapperDiv = styled.div`
-        text-align: center;
-        padding: 20px;
-        width: 100%;
-        font-size: 2em;
-    `
-
     return <WrapperDiv>
         <img src={loadingSpinner} />
     </WrapperDiv>
